35d1196895dc8868148ef6e553fc839400eb4e20
Rene Fischer <github@renefischer.org>
2020-10-25 12:29:43 +0100

    added missing /

--- test/isUrlAllowed.test.ts
+++ test/isUrlAllowed.test.ts
@@ -55,7 +55,7 @@ Deno.test("url starts with rule without trailing slash (rule: https://example.co
 });
 
 Deno.test("url starts with rule and trailing slash (rule: https://example.com/path1/, url: https://example.com/path1/12", () => {
-  const rules = "http://bar.xyz,https://foo.bar/path";
+  const rules = "http://bar.xyz,https://foo.bar/path/";
   const url = "https://foo.bar";
 
   assertEquals(isUrlAllowed(url + "/path", rules), true);
